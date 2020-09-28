2398db1ee0f77b994e898d5533cd1ed8bfbab49c
Sher Shah Arsalaie <shershah.arsalaie@hotmail.com>
2020-09-28 15:15:29 -0700

    implementing ejs file to print urls

--- express_server.js
+++ express_server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const app = express();
-app.set("view engine", "ejs");
-
 const PORT = 7000; // default port 8080
+
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -21,9 +21,10 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
  });
  
- app.get("/fetch", (req, res) => {
-  res.send(`a = ${a}`);
- });
+ app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
  
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
